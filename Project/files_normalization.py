2b274a5f53cce9301b605e3962e12d67a105ef2e
bkphn <bkphn011@gmail.com>
2026-03-21 20:59:52 +0100

    gitignore

--- Project/files_normalization.py
+++ Project/files_normalization.py
@@ -12,7 +12,7 @@ def normalize(language):
         with open(filename, 'r', encoding='utf-8') as file:
             data = file.readlines()
 
-        data = [re.sub(r'^\d+\.\s*', '', line).split('')[0] for line in data if line.strip()]
+        data = [re.sub(r'^\d+\.\s*', '', line).split(' ')[0] for line in data if line.strip()]
 
         with open(filename, 'w', encoding='utf-8') as file:
             file.write('\n'.join(data))
@@ -22,9 +22,37 @@ def normalize(language):
     except Exception as e:
         print(f"Wystąpił błąd: {e}")
 
+def is_cyrillic(text):
+    return bool(re.search(r'[\u0400-\u04FF]', text))
+
+def cyryllic_filter(language):
+    filename = os.path.join("Languages", language + ".txt")
+
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            words = file.read().splitlines()
+
+        data = []
+        for word in words:
+            if len(data) >= 15000:
+                break
+            elif is_cyrillic(word):
+                continue
+            else:
+                data.append(word)
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write('\n'.join(data))
+
+        print(f"Pomyślnie przefiltrowano {len(data)} słów, dla {language}.")
+    except Exception as e:
+        print(f"Wystąpił błąd: {e}")
+
 
 if __name__ == "__main__":
     running = True
+
+    # cyryllic_filter("serbian")
 
     while running:
         language = input("Podaj język: ")
